0693993e4e49ccae451f46b558da686d4450d165
Shane Coakley <shane.coakley3@mail.dcu.ie>
2017-03-24 16:48:21 +0000

    Rssfeed attempted .. doesn't seem to be working right

--- app/src/main/java/com/example/shane/final_project/News.java
+++ app/src/main/java/com/example/shane/final_project/News.java
@@ -1,13 +1,24 @@
 package com.example.shane.final_project;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
 
 public class News extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        RSSRead readRss = new RSSRead(this, recyclerView);
+        readRss.execute();
+
+
     }
 }
